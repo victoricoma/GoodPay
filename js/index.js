0a34132df57a2a4be3d0fa7836ac38c8f8bb0238
Leonardo Pires <ppires.leonardo@gmail.com>
2022-07-09 08:27:11 -0300

    Adição teste de calculo para verificação de robôs.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
-import userLogin from "./moduloDados.js";
-const alertaLogin = new bootstrap.Modal('#alertaLogin');
+import userLogin from"./moduloDados.js";
+const alertaLogin = new bootstrap.Modal("#alertaLogin");
 const entrarUsuario = document.getElementById("btnEntrar");
+//varial num1 e num2 aleatórias
+var num1 = Math.floor(Math.random() * 10);
+var num2 = Math.floor(Math.random() * 10);
+//soma das variáveis
+var resposta = num1 + num2;
+//imprime no site a fra "Qual a soma de num1 e num2"
+document.getElementById("teste").innerHTML = `Qual a soma de ${num1} + ${num2} ?`;
 
-entrarUsuario.addEventListener("click", _event =>{
-  let login = document.getElementById("login").value;
-  let senha = document.getElementById("senha").value;
-  let validaLogin = userLogin.find(userLogin => userLogin.userName === login);
-  if (login === validaLogin.userName && senha === validaLogin.senha) {
-    window.location.pathname="/app.html";
-  } else {
-    alertaLogin.show();
-  }
+//função para testar se está tudo correto após clicar no botão enviar
+entrarUsuario.addEventListener("click", _event => {
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
+    let validaLogin = userLogin.find(userLogin => userLogin.userName === login);
+    if (login === validaLogin.userName && senha === validaLogin.senha && document.getElementById("valida").value == resposta){
+            window.location.pathname="/app.html"
+        }
+    else {
+        alertaLogin.show();
+        }
 });
